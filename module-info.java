17a8bb538b5fb28f2692103e73bdddc6cca281cb
Neeraja <neerajasanapala309@gmail.com>
2024-11-26 00:40:41 -0500

    addedpgm

---
+++ module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author neera
+ *
+ */
+module Sample_Jenkins {
+}
---
+++ sample/Simple_Print.java
@@ -0,0 +1,15 @@
+package sample;
+
+public class Simple_Print {
+	
+	public static void main(String[] args)
+	{
+		int n=10;
+		
+		for(int i=1; i<=n ; i++)
+		{
+			System.out.println("i : "+ i);
+		}
+	}
+
+}
